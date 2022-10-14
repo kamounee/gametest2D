13342cc660ae526bb233443760a9e5a8e6ec635b
hamza <hamza.kamoune.job@gmail.com>
2022-10-14 02:44:50 +0100

    Game Loop

--- app/src/main/java/com/test/a2dgametst/MainActivity.java
+++ app/src/main/java/com/test/a2dgametst/MainActivity.java
@@ -2,13 +2,23 @@ package com.test.a2dgametst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        setContentView(new Game(this));
+
     }
 }
